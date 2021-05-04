1c96aaa5b13b0c7bee28f9e85943d720bb958ab9
Clark Evangelista <Clark Evangelista@DESKTOP-CFORFPG>
2021-05-04 21:00:59 +0900

    fix

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import Confetti from 'react-confetti';
 export default function Question(props){
     let [confetti,setConfetti] = useState(false);
